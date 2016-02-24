30a190aaa8200b52a007d32809a11fb7342a5038
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-02-24 17:44:03 -0300

    Added function to create CSS rules

--- src/fitembed.js
+++ src/fitembed.js
@@ -26,6 +26,7 @@
         this.intrinsic  = options.intrinsic  || true;
         this.baseWidth  = options.baseWidth  || 16;
         this.baseHeight = options.baseHeight || 9;
+        this.prefixes   = ['webkit', 'Moz', 'ms', 'O'];
 
         this.fit();
     };
@@ -40,14 +41,39 @@
             }
             return wrapper.appendChild(toWrap);
         },
+        vendor: function (el, prop) {
+            var s = el.style, pp, i, len;
+
+            prop = prop.charAt(0).toUpperCase() + prop.slice(1);
+
+            for (i = 0, len = this.prefixes.length; i < len; i += 1) {
+                pp = this.prefixes[i] + prop;
+
+                if(s[pp] !== undefined) return pp;
+            }
+
+            if(s[prop] !== undefined) return prop;
+        },
+        css: function (el, prop) {
+            var p;
+
+            for (p in prop) {
+                if (prop.hasOwnProperty(p)) {
+                    el.style[this.vendor(el, p) || p] = prop[p];
+                }
+            }
+        },
         fit: function () {
             var hRatio, width, height, mediaWrapper;
 
-            mediaWrapper                = document.createElement('div');
-            mediaWrapper.className      = 'media-wrapper';
-            mediaWrapper.style.position = 'relative';
-            mediaWrapper.style.width    = '100%';
-            mediaWrapper.style.height   = '0';
+            mediaWrapper           = document.createElement('div');
+            mediaWrapper.className = 'media-wrapper';
+
+            this.css(mediaWrapper, {
+                position: 'relative',
+                width: '100%',
+                height: '0'
+            });
 
             Array.prototype.forEach.call(this.embed, function (embed) {
                 width  = embed.getAttribute('width');
@@ -59,13 +85,17 @@
                     hRatio = (this.baseHeight / this.baseWidth) * 100;
                 }
 
-                mediaWrapper.style.padding = hRatio + '% 0 0 0';
+                this.css(mediaWrapper, {
+                    padding: hRatio + '% 0 0 0'
+                });
 
-                embed.style.position = 'absolute';
-                embed.style.width    = '100%';
-                embed.style.height   = '100%';
-                embed.style.top      = '0';
-                embed.style.left     = '0';
+                this.css(embed, {
+                    position: 'absolute',
+                    height: '100%',
+                    width: '100%',
+                    top: '0',
+                    left: '0'
+                });
 
                 this.wrap(embed, mediaWrapper);
             }.bind(this));
